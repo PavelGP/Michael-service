ed863e117dcd74241f356b3d49000955af8abb93
Pavel Gulevich <gulevichpavel@gmail.com>
2015-05-01 00:06:30 +0300

    add settings functions

---
+++ app/src/main/java/by/of/servicebook/myapplication/activities/TermsOfUseActivity.java
@@ -0,0 +1,49 @@
+package by.of.servicebook.myapplication.activities;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import by.of.servicebook.myapplication.R;
+
+public class TermsOfUseActivity extends ActionBarActivity {
+
+    public static void launch(Context context){
+        Intent intent = new Intent(context, TermsOfUseActivity.class);
+        context.startActivity(intent);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_terms_of_use);
+
+        setToolbar();
+    }
+
+    private void setToolbar(){
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == android.R.id.home) {
+            finish();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
--- app/src/main/java/by/of/servicebook/myapplication/fragments/SettingsFragment.java
+++ app/src/main/java/by/of/servicebook/myapplication/fragments/SettingsFragment.java
@@ -2,18 +2,29 @@ package by.of.servicebook.myapplication.fragments;
 
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import com.parse.LogOutCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
 
 import by.of.servicebook.myapplication.activities.MainActivity;
 import by.of.servicebook.myapplication.R;
+import by.of.servicebook.myapplication.activities.SplashActivity;
+import by.of.servicebook.myapplication.activities.TermsOfUseActivity;
 import by.of.servicebook.myapplication.utils.AppConst;
 
 
-public class SettingsFragment extends Fragment {
+public class SettingsFragment extends Fragment implements View.OnClickListener {
     private Activity activity;
 
     public static SettingsFragment newInstance() {
@@ -35,9 +46,87 @@ public class SettingsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+        View v = inflater.inflate(R.layout.fragment_settings, container, false);
+        ViewHolder viewHolder = new ViewHolder(v);
+
+        return v;
     }
 
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        switch (id){
+            case R.id.rlLogout:
+                showLogoutDialog();
+                break;
+            case R.id.rlTermsOfUse:
+                TermsOfUseActivity.launch(getActivity());
+                break;
+            case R.id.rlWriteUs:
+                sendEmail();
+                break;
+        }
+    }
 
+    private void showLogoutDialog(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setMessage(getString(R.string.warning_change_user))
+                .setCancelable(false)
+                .setNegativeButton(R.string.no,
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                            }
+                        })
+                .setPositiveButton(R.string.yes,
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                doLogout();
+                            }
+                        });
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+    private void doLogout(){
+
+        ParseUser.logOutInBackground(new LogOutCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e == null){
+                    SplashActivity.launch(getActivity());
+                } else {
+                    Toast.makeText(getActivity(), getString(R.string.logout_error),
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void sendEmail(){
+        Intent i = new Intent(Intent.ACTION_SEND);
+        i.setType("message/rfc822");
+        i.putExtra(Intent.EXTRA_EMAIL  , new String[]{getString(R.string.project_email)});
+        i.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.email_subject));
+        try {
+            startActivity(Intent.createChooser(i, getString(R.string.choose_email_client)));
+        } catch (android.content.ActivityNotFoundException ex) {
+            Toast.makeText(getActivity(), getString(R.string.no_email_clients), Toast.LENGTH_SHORT)
+                    .show();
+        }
+    }
+
+    private class ViewHolder{
+        RelativeLayout rlLogout, rlTermsOfUse, rlWriteUs;
+
+        public ViewHolder(View v){
+            rlLogout = (RelativeLayout) v.findViewById(R.id.rlLogout);
+            rlTermsOfUse = (RelativeLayout) v.findViewById(R.id.rlTermsOfUse);
+            rlWriteUs = (RelativeLayout) v.findViewById(R.id.rlWriteUs);
+
+            rlLogout.setOnClickListener(SettingsFragment.this);
+            rlTermsOfUse.setOnClickListener(SettingsFragment.this);
+            rlWriteUs.setOnClickListener(SettingsFragment.this);
+        }
+    }
 }
